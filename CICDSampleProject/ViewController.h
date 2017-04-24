8fcaf316d6842b321f8b596fbd201ad58b6878f8
Shivaji Tanpure <shivaji.tanpure@cuelogic.com>
2017-04-24 13:56:02 +0530

    first commit

---
+++ CICDSampleProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CICDSampleProject
+//
+//  Created by Shivaji Tanpure on 24/04/17.
+//  Copyright © 2017 Cuelogic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
